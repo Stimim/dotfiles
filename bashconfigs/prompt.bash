67af3d9fdc0d350f7f7289e080cb44450d84af1b
Stimim <death1048576@gmail.com>
2016-10-01 23:54:55 +0800

    improve portability
    1. use "command -v <command>" to check if a command exists
    2. use "#!/usr/bin/env bash" rather than "/bin/bash"

--- bashconfigs/prompt.bash
+++ bashconfigs/prompt.bash
@@ -1,4 +1,4 @@
-if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+if command -V tput && tput setaf 1 >&/dev/null; then
   # We have color support; assume it's compliant with Ecma-48
   # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
   # a case would tend to support setf rather than setaf.)
