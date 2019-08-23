c5304100f06e8461b4ddb84843686957268bbba8
Stimim <death1048576@gmail.com>
2019-08-23 14:49:53 +0800

    alias: repo upload: don't set 'topic'

--- bashconfigs/custom/aliases.bash
+++ bashconfigs/custom/aliases.bash
@@ -2,5 +2,5 @@ alias ptt='ssh -C bbsu@ptt.cc'
 alias ptt2='ssh -C bbsu@ptt2.cc'
 alias Nethack='ssh nethack@alt.org'
 
-alias ru='repo upload . -t --cbr'
+alias ru='repo upload . --cbr'
 alias rr='repo rebase .'
