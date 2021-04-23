de3247d79e9bb981a6402ff49a91a6fbc2979e0f
Stimim Chen <106878+Stimim@users.noreply.github.com>
2021-04-23 03:47:23 +0000

    vimconfig: setup.sh: detect nvim

--- vimconfigs/setup.sh
+++ vimconfigs/setup.sh
@@ -12,4 +12,10 @@ ln -sf ~/.vim/stimim/vimrc.vim ~/.vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-vim -u ~/.vim/stimim/bundles.vim +PlugInstall +qall
+if which nvim; then
+  mkdir -p ~/.config/nvim/
+  ln -sf ~/.vimrc ~/.config/nvim/init.vim
+  nvim -u ~/.vim/stimim/bundles.vim +PlugInstall +qall
+else
+  vim -u ~/.vim/stimim/bundles.vim +PlugInstall +qall
+fi
