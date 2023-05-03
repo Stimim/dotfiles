1a6414faa31dad193b164ee4d912042cbfb5dc20
Stimim Chen <stimim@google.com>
2023-05-03 14:15:04 +0800

    2023-05-03

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -3,6 +3,6 @@
 PWD=`pwd`
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-ln -sf $PWD/tmux.conf ~/.tmux.conf
+ln -sf "${PWD}/tmux.conf" ~/.tmux.conf
 
 echo "In tmux, press <prefix>+I to install plugins"
